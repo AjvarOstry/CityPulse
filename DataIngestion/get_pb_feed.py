cbfea287873cac6c2a4c6580b3aa91b8679efd7f
Marcin Cieślak <another.long.nickname@gmail.com>
2026-06-30 18:21:37 +0200

    feat(DataIngestion): Fisrt try to get and display realtime gtfs data

---
+++ DataIngestion/get_pb_feed.py
@@ -0,0 +1,21 @@
+
+from google.transit import gtfs_realtime_pb2
+import requests
+
+URL = "https://cdn.zbiorkom.live/gtfs-rt/lodz.pb"
+def get_pb_feed():
+
+    pb_feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get(URL)
+    pb_feed.ParseFromString(response.content)
+    return pb_feed
+
+
+
+
+if __name__ == "__main__":
+
+    for entity in get_pb_feed().entity:
+        if entity.HasField('trip_update'):
+            print(entity.trip_update)
+            print(entity.vehic)
